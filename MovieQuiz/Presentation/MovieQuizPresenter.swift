62909eb4d3939d56e835383cc6c3f3540222309f
Vlad Kramskoy <vkto5609@gmail.com>
2023-11-27 22:33:29 +0300

    [refactor] Рефакторинг функции showNextQuestionOrResults

--- MovieQuiz/Presentation/MovieQuizPresenter.swift
+++ MovieQuiz/Presentation/MovieQuizPresenter.swift
@@ -2,17 +2,20 @@ import UIKit
 
 final class MovieQuizPresenter {
     let questionAmount: Int = 10
-    private var currentQuestionIndex = 0
-    
+    var currentQuestionIndex = 0
     var currentQuestion: QuizQuestion?
     weak var viewController: MovieQuizViewController?
+    
+    var correctAnswers = 0
+    var questionFactory: QuestionFactoryProtocol?
     
     func isLastQuestion() -> Bool {
         currentQuestionIndex == questionAmount - 1
     }
     
-    func resetQuestionIndex() {
+    func restartGame() {
         currentQuestionIndex = 0
+        correctAnswers = 0
     }
     
     func switchToNextQuestion() {
@@ -27,19 +30,86 @@ final class MovieQuizPresenter {
         return questionStep
     }
     
-    func yesButtonClicked(_ sender: UIButton) {
-        guard let currentQuestion = currentQuestion else {
+    func didReceiveNextQuestion(question: QuizQuestion?) {
+        guard let question = question else {
             return
         }
-        let givenAnswer = true
-        viewController?.showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
+        
+        currentQuestion = question
+        let viewModel = convert(model: question)
+        DispatchQueue.main.async { [weak self] in
+            self?.viewController?.show(quiz: viewModel)
+        }
+    }
+    
+    func showQuestionOrResults() {
+        // Блок показывает результат
+        if self.isLastQuestion() {
+            
+            // handler (в замыканиях обычно использовались weak self, нужно подобное применять здесь???)
+            let repeatQuiz: ((UIAlertAction) -> Void)? = repeatQuiz
+            guard let handler = repeatQuiz else { return }
+            
+            // Вызываем метод сохранения сезультата
+                viewController?.statistic?.store(correct: correctAnswers, total: questionAmount)
+            
+            // Безопасно извлекаем рекорд и счетчик игр из User Defaults
+            guard let recordGet = viewController?.statistic?.bestGame,
+                  let gameCountGet = viewController?.statistic?.gamesCount,
+                  let totalAccuracyGet = viewController?.statistic?.totalAccuracy
+            else {
+                return
+            }
+            // Конвертируем сохраненную дату рекорда в нужный формат
+            let recordDate = recordGet.date
+            let dateString = recordDate.dateTimeString
+            
+            let resultAlert = AlertModel(title: "Этот раунд окончен!",
+                                         message: """
+                                         Ваш результат: \(correctAnswers)/\(questionAmount)
+                                         Количество сыгранных квизов: \(gameCountGet)
+                                         Рекорд: \(recordGet.correct)/\(recordGet.total) (\(dateString))
+                                         Средняя точность: \(String(format: "%.2f", totalAccuracyGet))%
+                                         """,
+                                         buttonText: "Сыграть ещё раз",
+                                         completion: handler) // Экз. модели
+            
+            guard let alert = viewController?.alertPresenter?.show(model: resultAlert) else { return } // Экз. класса
+            
+            viewController?.present(alert, animated: true, completion: nil) // Вызов алерта
+        } else {
+            // Блок показывает след. вопрос
+            self.switchToNextQuestion()
+            
+            questionFactory?.requestNextQuestion()
+        }
+    }
+    
+    // repeatQuiz нужен чтобы создать handler
+    private func repeatQuiz(action: UIAlertAction) {
+        self.restartGame()
+        self.questionFactory?.requestNextQuestion()
+    }
+    
+    func yesButtonClicked(_ sender: UIButton) {
+        didAnswer(isCorrectAnswer: true)
     }
     
     func noButtonClicked(_ sender: UIButton) {
+        didAnswer(isCorrectAnswer: false)
+    }
+    
+    func didAnswer(isCorrectAnswer: Bool) {
         guard let currentQuestion = currentQuestion else {
             return
         }
-        let givenAnswer = false
+        
+        let givenAnswer = isCorrectAnswer
+        
+        if isCorrectAnswer {
+            correctAnswers += 1
+        }
+        
         viewController?.showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
     }
 }
--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -3,11 +3,9 @@ import UIKit
 final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate, StatisticServiceDelegate {
     
     private let presenter = MovieQuizPresenter()
-    private var questionFactory: QuestionFactoryProtocol?
-    private var currentQuestion: QuizQuestion?
-    private var correctAnswers = 0
-    private var alertPresenter: AlertPresenterProtocol?
-    private var statistic: StatisticServiceProtocol?
+    private var questionFactory: QuestionFactoryProtocol? // дубли
+    var alertPresenter: AlertPresenterProtocol?
+    var statistic: StatisticServiceProtocol?
     
     @IBOutlet private weak var imageView: UIImageView!
     @IBOutlet private weak var textLabel: UILabel!
@@ -42,15 +40,7 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate, 
     // MARK: - QuestionFactoryDelegate
     
     func didReceiveNextQuestion(question: QuizQuestion?) {
-        guard let question = question else {
-            return
-        }
-        
-        currentQuestion = question
-        let viewModel = presenter.convert(model: question)
-        DispatchQueue.main.async { [weak self] in
-            self?.show(quiz: viewModel)
-        }
+        presenter.didReceiveNextQuestion(question: question)
     }
     
     // MARK: - Private functions
@@ -73,8 +63,7 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate, 
                                buttonText: "Попробовать ещё раз") { [weak self] _ in
             guard let self = self else { return }
             
-            self.presenter.resetQuestionIndex()
-            self.correctAnswers = 0
+            self.presenter.restartGame()
             
             showLoadingIndicator()
             questionFactory?.loadData()
@@ -84,59 +73,13 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate, 
         present(alert, animated: true, completion: nil)
     }
     
-    private func show(quiz step: QuizStepViewModel) {
+    func show(quiz step: QuizStepViewModel) {
         imageView.image = step.image
         textLabel.text = step.question
         counterLabel.text = step.questionNumber
     }
     
-    private func showQuestionOrResults() {
-        // Блок показывает результат
-        if presenter.isLastQuestion() {
-            
-            // handler (в замыканиях обычно использовались weak self, нужно подобное применять здесь???)
-            let repeatQuiz: ((UIAlertAction) -> Void)? = repeatQuiz
-            guard let handler = repeatQuiz else { return }
-            
-            // Вызываем метод сохранения сезультата
-            statistic?.store(correct: correctAnswers, total: presenter.questionAmount)
-            
-            // Безопасно извлекаем рекорд и счетчик игр из User Defaults
-            guard let recordGet = statistic?.bestGame,
-                  let gameCountGet = statistic?.gamesCount,
-                  let totalAccuracyGet = statistic?.totalAccuracy
-            else {
-                return
-            }
-            // Конвертируем сохраненную дату рекорда в нужный формат
-            let recordDate = recordGet.date
-            let dateString = recordDate.dateTimeString
-            
-            let resultAlert = AlertModel(title: "Этот раунд окончен!",
-                                         message: """
-                                         Ваш результат: \(correctAnswers)/\(presenter.questionAmount)
-                                         Количество сыгранных квизов: \(gameCountGet)
-                                         Рекорд: \(recordGet.correct)/\(recordGet.total) (\(dateString))
-                                         Средняя точность: \(String(format: "%.2f", totalAccuracyGet))%
-                                         """,
-                                         buttonText: "Сыграть ещё раз",
-                                         completion: handler) // Экз. модели
-            
-            guard let alert = alertPresenter?.show(model: resultAlert) else { return } // Экз. класса
-            
-            self.present(alert, animated: true, completion: nil) // Вызов алерта
-        } else {
-            // Блок показывает след. вопрос
-            presenter.switchToNextQuestion()
-            
-            self.questionFactory?.requestNextQuestion()
-        }
-    }
-    
     func showAnswerResult(isCorrect: Bool) {
-        if isCorrect {
-            correctAnswers += 1
-        }
 
         imageView.layer.borderWidth = 8
         imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
@@ -150,15 +93,9 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate, 
             // Убираем рамку
             self.imageView.layer.borderWidth = 0
             // Показываем след. вопрос
-            self.showQuestionOrResults()
+            self.presenter.questionFactory = self.questionFactory
+            self.presenter.showQuestionOrResults()
         }
-    }
-    
-    // repeatQuiz нужен чтобы создать handler
-    private func repeatQuiz(action: UIAlertAction) {
-        self.presenter.resetQuestionIndex()
-        self.correctAnswers = 0
-        self.questionFactory?.requestNextQuestion()
     }
     
     private func enableButtons(enable: Bool) {
@@ -185,25 +122,10 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate, 
     // MARK: - Actions
     
     @IBAction private func yesButtonClicked(_ sender: UIButton) {
-        presenter.currentQuestion = currentQuestion
         presenter.yesButtonClicked(yesButton)
     }
-//    @IBAction private func yesButtonClicked(_ sender: UIButton) {
-//        guard let currentQuestion = currentQuestion else {
-//            return
-//        }
-//        let givenAnswer = true
-//        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
-//    }
+
     @IBAction private func noButtonClicked(_ sender: UIButton) {
-        presenter.currentQuestion = currentQuestion
         presenter.noButtonClicked(noButton)
     }
-//    @IBAction private func noButtonClicked(_ sender: UIButton) {
-//        guard let currentQuestion = currentQuestion else {
-//            return
-//        }
-//        let givenAnswer = false
-//        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
-//    }
 }
